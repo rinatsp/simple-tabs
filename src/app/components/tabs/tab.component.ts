06f17682d4a69dcd649c37c172f714d48b635a4f
Rinat <rinat95.95@yandex.ru>
2018-08-24 09:01:22 +0300

    Create simple task

---
+++ src/app/components/tabs/tab.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'tab',
+  styles: [
+    `
+    .pane{
+      padding: 1em;
+    }
+  `
+  ],
+  template: `
+    <div [hidden]="!active" class="pane">
+      <ng-content *ngIf="!template"></ng-content>
+      <ng-container *ngIf="template"
+        [ngTemplateOutlet]="template"
+        [ngTemplateOutletContext]="{data: dataContext}">
+      </ng-container>
+    </div>
+  `
+})
+export class TabComponent {
+  @Input() tabTitle: string;
+  @Input() active = false;
+  @Input() template;
+  @Input() dataContext;
+}
