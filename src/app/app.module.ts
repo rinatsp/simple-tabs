0f11afabe0c366b8862a2e6ae548a9fea3e9fd05
Rinat <rinat95.95@yandex.ru>
2018-08-24 09:00:32 +0300

    tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { TestComponent } from './components/test/test.component';
+import {TabComponent} from "./components/tabs/tab.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    TabComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
